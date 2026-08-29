12636b74d59fb6c99f50cf053813faab8309d8ad
St. Lazarus' Media Unit - Sri Lanka <stlazaruskurukulawaofficial@gmail.com>
2026-08-29 11:05:07 +0530

    Modify notification messages in index-script.js
    Updated notification messages to reflect project status.

--- JS/index-script.js
+++ JS/index-script.js
@@ -75,7 +75,7 @@ function createNotifications() {
   
   const notifications = [
     { message: "This site is a project by Lazmedia. and still in development." },
-    { message: "Developed and Maintained by Alpha Studios UK pvt Ltd." } //let users know about the developer.
+    { message: "Some functionsmay not work properly" } //let users know about the developer.
   ];
 
   notifications.forEach((notification, index) => {
